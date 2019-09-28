ce7f503d21f4b72c88a04f5007f12472b2b536a1
AV-GitHubcom <av.newdialog@gmail.com>
2019-09-29 00:56:48 +0300

    updated models 29.09.2019 00.56.00

--- app/models.py
+++ app/models.py
@@ -12,10 +12,11 @@ class Order(models.Model):
     name_product = models.ForeignKey('Product', related_name='product', verbose_name='Меню',
                                      null=True, on_delete=models.CASCADE)
     # product = models.TextField(verbose_name='Заказ')
-    count = models.IntegerField(verbose_name='Количество')
+    count = models.IntegerField(verbose_name='Количество', default=1)
     # paid = models.IntegerField(verbose_name='Оплачено')
-    price = models.ForeignKey('Product', related_name='paid', verbose_name='Оплачено', on_delete=models.PROTECT,
-                              default=0)
+    # price = models.ForeignKey('Product', related_name='paid', verbose_name='Оплачено', on_delete=models.PROTECT,
+    #                           default='name_product.price')
+    price = models.IntegerField(verbose_name='Оплачено')
     comment = models.TextField(verbose_name='Комментарий')
 
     class Meta:
