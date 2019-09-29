afa297c4363621755048b48dd6888c68db20405c
Kate <katerina_voitko@gmail.com>
2019-09-29 22:49:05 +0300

    change views, look in comments

--- app/views.py
+++ app/views.py
@@ -9,20 +9,20 @@ class MenuListView(generic.ListView):
     queryset = Product.objects.all()
     template_name = 'index.html'
 
-class PostDetail(generic.DetailView):
-    model = Order_detail.html
-    template_name = 'post_detail.html'
-
-def add_item_to_order(request, slug):
-    order = get_object_or_404(Order, slug=slug)
-    if request.method == "POST":
-        form = OrderForm(request.POST)
-        if form.is_valid():
-            order = form.save(commit=False)
-            order.post = order
-            order.save()
-            return redirect('order_detail', slug=order.slug)
-    else:
-        form = OrderForm()
-    return render(request, 'order_detail.html', {'form': form})
+# class PostDetail(generic.DetailView):
+#     model = Order
+#     template_name = 'order_detail.html'
+#
+# def add_item_to_order(request, slug):
+#     order = get_object_or_404(Order, slug=slug)
+#     if request.method == "POST":
+#         form = OrderForm(request.POST)
+#         if form.is_valid():
+#             order = form.save(commit=False)
+#             order.post = order
+#             order.save()
+#             return redirect('order_detail', slug=order.slug)
+#     else:
+#         form = OrderForm()
+#     return render(request, 'order_detail.html', {'form': form})
 
