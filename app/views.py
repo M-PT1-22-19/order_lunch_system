3a1381bb108fcd35867b3f9b20976e7513002aa1
Kate <katerina_voitko@gmail.com>
2019-09-29 22:38:44 +0300

    change views

--- app/views.py
+++ app/views.py
@@ -1,3 +1,4 @@
+from django.shortcuts import get_object_or_404, redirect, render
 from django.views import generic
 from app.models import Order
 
@@ -7,3 +8,21 @@ from app.models import Order
 class MenuListView(generic.ListView):
     queryset = Order.objects
     template_name = 'index.html'
+
+class PostDetail(generic.DetailView):
+    model = Order_detail.html
+    template_name = 'post_detail.html'
+
+def add_item_to_order(request, slug):
+    order = get_object_or_404(Order, slug=slug)
+    if request.method == "POST":
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order = form.save(commit=False)
+            order.post = order
+            order.save()
+            return redirect('order_detail', slug=order.slug)
+    else:
+        form = OrderForm()
+    return render(request, 'order_detail.html', {'form': form})
+
