dac8816b04bd1631d1fee93e874321bdb436ce03
nadezhda <nadezhdaboltach@gmail.com>
2019-10-03 21:50:22 +0300

    updated by dzmitry

--- app/forms.py
+++ app/forms.py
@@ -1,12 +1,14 @@
 from django import forms
 from .models import Product, Order, CheckList
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
 class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
-        fields = ('name_product', 'price',)
+        fields = ('slug', 'name_product', 'count', 'price',)
 
 
 class OrderForm(forms.ModelForm):
@@ -21,3 +23,11 @@ class CheckListForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = ('name_product', 'user', 'summary', 'comment',)
+
+
+class AdminRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2',]
