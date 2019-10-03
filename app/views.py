26672080020f416e2a17a7c3d06b96a5eb2733b1
nadezhda <nadezhdaboltach@gmail.com>
2019-10-03 21:50:37 +0300

    updated by dzmitry

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from app.models import Product, CheckList
-from .forms import OrderForm, CheckListForm
+from .forms import OrderForm, CheckListForm, AdminRegisterForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -13,27 +13,30 @@ def add_item_to_order(request):
         form = OrderForm(request.POST)
         if form.is_valid():
             product_uid = form.data['product_uid']
+            product_count = int(request.POST[product_uid])
             product = Product.objects.get(slug__iexact=product_uid)
             order = form.save(commit=False)
             order.name_product = product
-            order.summary = order.count * product.price
+            order.count = product_count
+            order.summary = product_count * product.price
             order.comment = "введен при заказе"
             order.save()
 
-        return render(request, 'index.html', {'product_list': queryset})
+        return render(request, 'base.html', {'product_list': queryset})
     else:
-        return render(request, 'index.html', {'product_list': queryset})
+        return render(request, 'base.html', {'product_list': queryset})
 
 
 def register(request):
     if request.method == 'POST':
         form = UserCreationForm(request.POST)
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
+            # messages.success(request, f'Account created for {username}!')
             return redirect('/admin/')
     else:
-        form = UserCreationForm()
+        form = AdminRegisterForm()
     return render(request, 'app/register.html', {'form': form})
 
 
