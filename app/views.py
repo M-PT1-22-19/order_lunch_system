8ed081a8ba85b635d330640e7d7cbdcf19e817d0
M-PT1-22-19 <M-PT1-22-19@mail.u>
2019-10-04 16:15:06 +0300

    turned off unused or unready views

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from app.models import Product, CheckList
-from .forms import OrderForm, CheckListForm, AdminRegisterForm
+from app.models import Product, Order#, CheckList
+from .forms import OrderForm, OrderListForm, AdminRegisterForm
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 
@@ -19,26 +19,30 @@ def add_item_to_order(request):
             order.name_product = product
             order.count = product_count
             order.summary = product_count * product.price
-            order.comment = "введен при заказе"
+            # order.comment = "введен при заказе: " + order.comment
             order.save()
 
         return render(request, 'base.html', {'product_list': queryset})
     else:
         return render(request, 'base.html', {'product_list': queryset})
 
-def show_checklist(request):
-    queryset = CheckList.objects.all
-    if request.method == "POST":
-        form = CheckListform(request.POST)
-        if form.is_valid():
-            table = form.save(commit=False)
-            table.user = form.user
-            table.name_product = form.name_product
-            table.summary = form.summary
-            table.comment = form.comment
-        return render(request, 'Order.html', {'order_list': queryset})
-    else:
-        return render(request, 'Order.html', {'order_list': queryset})
+
+# show_checklist renamed to show_order_list
+def show_order_list(request):
+    queryset = Order.objects.all()
+    ## added to comment, because there is no request yet
+    # if request.method == "POST":
+    #     form = CheckListform(request.POST)
+    #     if form.is_valid():
+    #         table = form.save(commit=False)
+    #         table.user = form.user
+    #         table.name_product = form.name_product
+    #         table.summary = form.summary
+    #         table.comment = form.comment
+    #     return render(request, 'Order.html', {'order_list': queryset})
+    # else:
+    return render(request, 'app/order_list.html', {'order_list': queryset})
+
 
 def register(request):
     if request.method == 'POST':
@@ -46,23 +50,10 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            # messages.success(request, f'Account created for {username}!')
-            return redirect('/admin/')
+            messages.success(request, f'Account created for {username}!')
+            return redirect('/home/')
+        else:
+            return redirect('/order_list')
     else:
         form = AdminRegisterForm()
     return render(request, 'app/register.html', {'form': form})
-
-
-# def show_checklist(request):
-#     queryset = CheckList.objects.all()
-#     if request.method == 'POST':
-#         form = CheckListForm(request.POST)
-#         if form.is_valid():
-#             table = form.save(commit=False)
-#             table.name_product = form.name_product
-#             table.user = form.user
-#             table.summury = form.summary
-#             table.comment = form.comment
-#         return render(request, 'order.html', {'order_list': queryset})
-#     else:
-#         return render(request, 'order.html', {'order_list': queryset})
