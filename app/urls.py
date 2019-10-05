21be02795101fbf82189532c09265818be805af4
nadezhda <nadezhdaboltach@gmail.com>
2019-10-05 12:25:56 +0300

    NOT READY YET

--- app/urls.py
+++ app/urls.py
@@ -1,8 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
-    path('', views.add_item_to_order, name='home'),
+    path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('', views.add_item_to_order, name='home'),
     path('order_list/', views.show_order_list, name='order_list'),
 ]
