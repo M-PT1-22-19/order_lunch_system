58aa35b168c60a379cff82d1e6407d2e5c22e7fc
M-PT1-22-19 <M-PT1-22-19@mail.ru>
2019-10-07 23:43:45 +0300

    dmitry: authentication stuff

--- app/urls.py
+++ app/urls.py
@@ -1,9 +1,12 @@
 from . import views
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
+# from users import views as user_views
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
     path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
     path('', views.add_item_to_order, name='home'),
     path('order_list/', views.show_order_list, name='order_list'),
 ]
